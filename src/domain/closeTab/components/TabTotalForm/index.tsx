904538d9274fe5668f5fec6216f97377b255ee3e
Adalberto Sampaio <adalberto.sampaio@grancursosonline.com.br>
2024-04-10 15:52:43 -0300

    fix: Corrects inputmode

--- src/domain/closeTab/components/TabTotalForm/index.tsx
+++ src/domain/closeTab/components/TabTotalForm/index.tsx
@@ -42,7 +42,7 @@ const TabTotalForm: FunctionComponent<TabTotalFormProps> = ({
         control={control}
         preffix="R$"
         type="text"
-        inputMode="numeric"
+        inputMode="decimal"
         pattern="[0-9]*"
       />
     </Form>
